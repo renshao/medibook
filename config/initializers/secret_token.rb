7451bd9fa0f0cf83625f0a9555c5161f35cdcbb4
Ren Shao <ren@shao.io>
2014-03-21 15:11:11 +1100

    Created new rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BabyHealth::Application.config.secret_key_base = '9e7b3511ae4197f972f9379bf37548999b4ffef5556b5162fd5859dc94da38dbe228babd08a442ec525bf82186ce86b7d45e0adf0f1e7075828ac55a90cbf40c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BabyHealth::Application.config.session_store :cookie_store, key: '_baby-health_session'
